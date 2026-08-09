e1b845e73aa543cec9245e7421ce17db0f088b4f
HoangKhang <nguyenkhang.03012006@gmail.com>
2026-08-09 18:10:54 +0700

    build: bootsnap Spring Boot backend

---
+++ backend/src/main/java/com/ryannguyxn/smartshareexpensehub/SmartShareExpenseHubApplication.java
@@ -0,0 +1,13 @@
+package com.ryannguyxn.smartshareexpensehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartShareExpenseHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartShareExpenseHubApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ryannguyxn/smartshareexpensehub/SmartShareExpenseHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryannguyxn.smartshareexpensehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartShareExpenseHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
